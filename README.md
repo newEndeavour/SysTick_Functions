c135c7d8300584c4633235e898d9660ac6ca7444
newEndavour <36076458+newEndeavour@users.noreply.github.com>
2019-03-19 12:03:27 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 #Systick_Functions Library#
 
+The SysTick_Functions library for Arduino allows implementation of delays under a microsecond.
+
 https://github.com/newEndeavour/Systick_Functions
 
 any question, please feel free to ask: jerome.p.drouin@gmail.com
--- Systick_Functions.h
+++ Systick_Functions.h
@@ -7,6 +7,11 @@
 
   https://github.com/newEndeavour/Systick_Functions
 
+  The SysTick_Functions library for Arduino allows implementation of delays 
+  under a microsecond. Note: The functions will not work, and should not 
+  be used in situations where the required delay exceeds or is close to a 
+  millisecond, i.e. 1000 micros). 
+
   Copyright (c) 2018-2019 Jerome Drouin  All rights reserved.
 
   This program is free software: you can redistribute it and/or modify
