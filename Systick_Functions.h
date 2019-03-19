6167a247c4336545e388c8fbb1f4e3f2fd15be1f
newEndavour <36076458+newEndeavour@users.noreply.github.com>
2019-03-19 12:12:41 +0000

    Add files via upload

--- Systick_Functions.h
+++ Systick_Functions.h
@@ -8,9 +8,13 @@
   https://github.com/newEndeavour/Systick_Functions
 
   The SysTick_Functions library for Arduino allows implementation of delays 
-  under a microsecond. Note: The functions will not work, and should not 
-  be used in situations where the required delay exceeds or is close to a 
-  millisecond, i.e. 1000 micros). 
+  under a microsecond, or in situations where the sampling granularity of the 
+  the function delayMicroseconds (4 mic) can present a problem, notably, for 
+  capacitive sensors measuring very small capacitance and where 4 micros error 
+  is not acceptable.
+  WARNING: The functions will not work, and should not be used in situations 
+  where the required/measured delay exceeds or is close to a millisecond, 
+  i.e. 1000 micros. 
 
   Copyright (c) 2018-2019 Jerome Drouin  All rights reserved.
 
