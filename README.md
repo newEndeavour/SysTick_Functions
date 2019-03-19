35c146662caec60c4adc0322a2578a3b9e5b0c1f
newEndavour <36076458+newEndeavour@users.noreply.github.com>
2019-03-19 11:18:36 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+#Systick_Functions Library#
+
+https://github.com/newEndeavour/Systick_Functions
+
+any question, please feel free to ask: jerome.p.drouin@gmail.com
+
---
+++ Systick_Functions.h
@@ -0,0 +1,87 @@
+/*
+  File:         Systick_Functions.h
+  Version:      0.0.1
+  Date:         19-Mar-2019
+  Revision:     19-Mar-2019
+  Author:       Jerome Drouin (jerome.p.drouin@gmail.com)
+
+  https://github.com/newEndeavour/Systick_Functions
+
+  Copyright (c) 2018-2019 Jerome Drouin  All rights reserved.
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+ 
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  Editions:
+  - 0.0.1	: First version
+  
+*/
+
+
+// ensure this library description is only included once
+#ifndef Systick_Functions_h
+#define Systick_Functions_h
+
+#if ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+
+// delayMicroseconds_SysTick returns ticks
+int delayMicroseconds_SysTick(int tmicros) {
+int ati=tmicros * (SysTick->LOAD+1) / 1000 - 300;
+int sta,sto,tik;
+
+  if (tmicros>999)
+    return -1;
+    
+  sta = SysTick->VAL;
+  do {
+    sto = SysTick->VAL;
+    if (sto>sta)
+      sta += SysTick->LOAD;
+    tik = sta - sto;
+  } while( tik < ati);
+    
+  //return ticks  
+  return tik;       
+}
+
+
+
+// delaySysTick, returns time in microseconds
+float delaySysTick(int ticks) {
+int ati=ticks;
+int sta,sto,tik;
+
+  if (ticks>SysTick->LOAD)
+    return -1;
+    
+  sta = SysTick->VAL;
+  do {
+    sto = SysTick->VAL;
+    if (sto>sta)
+      sta += SysTick->LOAD;
+    tik = sta - sto;
+  } while( tik < ati);
+
+  //return ticks
+  return tik / ((SysTick->LOAD+1) / 1000);       
+}
+  
+#endif
+//EndofFile
+
+
